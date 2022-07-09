52cfcb3a3a0a2f570c84d5ba5c310fd8898ce178
AshitJ1 <Ashit@oneforlife.in>
2022-07-09 17:42:32 +0530

    product card  fixed

--- src/app/pages/mainscr/product.service.ts
+++ src/app/pages/mainscr/product.service.ts
@@ -9,8 +9,8 @@ export class ProductService {
   products: Product[] = [
     new Product (1, 'Apple iPhone 28 12 1GB', 45000, 'assets/Images/igraph.png'),
     new Product (2, 'Men White Fit Sweatshirt', 2000, 'assets/Images/hm.jpg'),
-    new Product (3, 'Apple watch', 15000, 'assets/Images/aplwch.png'),
-    new Product (4, 'Apple iPhone 28 12 1GB', 45000),
+    new Product (3, 'IKIGAI PaperBack', 300, 'assets/Images/ikigai.jpg'),
+    new Product (4),
   ]
 
   constructor() { }
