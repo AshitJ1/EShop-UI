fd958761954e9996957ea28aaabc5dc6e5feb159
AshitJ1 <Ashit@oneforlife.in>
2022-09-03 18:36:28 +0530

    spalsh changed

--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loader',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) {  
+    setTimeout(() => {
+    this.router.navigateByUrl('login');
+  }, 1000);
+} 
+ 
 
   ngOnInit() {
   }
